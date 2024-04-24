b1df7b5359b9084756b07d879b6f1c8d381ce087
Tate Myers <tatmyers@iastate.edu>
2024-04-23 19:00:37 -0500

    TATET

--- backend/index.js
+++ backend/index.js
@@ -23,3 +23,103 @@ const host = "localhost";
 app.listen(port, () => {
   console.log("App listening at http://%s:%s", host, port);
 });
+
+
+app.get("/", async (req, res) => {
+  await client.connect();
+  console.log("Node connected successfully to GET MongoDB");
+  const query = {};
+  const results = await db
+  .collection("fakestore_catalog")
+  .find(query)
+  .limit(100)
+  .toArray();
+  console.log(results);
+  res.status(200);
+});
+
+app.get("/:id", async (req, res) => {
+  const itemid = Number(req.params.id);
+  console.log("Item to find :", itemid);
+  await client.connect();
+  console.log("Node connected successfully to GET-id MongoDB");
+  const query = {"id": itemid };
+  const results = await db.collection("fakestore_catalog")
+  .findOne(query);
+  console.log("Results :", results);
+  if (!results) res.send("Not Found").status(404);
+  else res.send(results).status(200);
+ });
+    
+app.post("/addItem", async (req, res) => {
+  try{
+    await client.connect();
+    const keys = Object.keys(req.body);
+    const values = Object.values(req.body);
+    const newDocument = {
+      "id": values[0], // also "id": req.body.id,
+      "title": values[1], // also "name": req.body.name,
+      "price": values[2], // also "price": req.body.price,
+      "description": values[3], // also "description": req.body.description,
+      "category": values[4], // also "imageUrl": req.body.imageUrl
+      "image": values[5],
+      "rating": {
+        "rate": values[6],
+        "count": values[7]
+      }
+    };
+    console.log(newDocument);
+
+    const results = await db
+    .collection("fakestore_catalog")
+    .insertOne(newDocument);
+    res.status(200);
+    res.send(results);
+  }
+  catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).send({ error: 'An internal server error occurred' });
+  }
+
+ });
+
+app.delete("/deleteItem/:id", async (req, res) => {
+  try {
+  const id = Number(req.params.id);
+  await client.connect();
+  console.log("Item to delete :",id);
+  const query = { id: id };
+  // delete
+  const results = await db.collection("fakestore_catalog").deleteOne(query);
+  res.status(200);
+  res.send(results);
+
+  }
+  catch (error){
+  console.error("Error deleting item:", error);
+  res.status(500).send({ message: 'Internal Server Error' });
+  }
+  });
+
+  app.put("/updateItem/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    const query = { id: id };
+    await client.connect();
+    console.log("Item to Update :",id);
+    // Data for updating the document, typically comes from the request body
+    console.log(req.body);
+    const updateData = {
+    $set:{
+    "price": req.body.price,
+    }
+    };
+    // Add options if needed, for example { upsert: true } to create a document if it doesn't exist
+    const options = { };
+    const results = await db.collection("fakestore_catalog").updateOne(query, updateData, options);
+    res.status(200);
+    res.send(results);
+    if (results.matchedCount === 0) {
+      return res.status(404).send({ message: 'Item not found' });
+    }
+    });
+ 
