fbcb55462766ac248d2a5fec11d1b12971ec0a61
Tate Myers <tatmyers@iastate.edu>
2024-04-26 12:56:14 -0500

    ahhh

--- frontend/src/components/UpdateProduct.js
+++ frontend/src/components/UpdateProduct.js
@@ -1,4 +1,28 @@
 
 export function UpdateProduct() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-}
+    const onSubmit = async (data) => {
+        const unique_id = uuid();
+        const small_id = unique_id.slice(0, 8);
+
+        data.id = small_id;
+        
+        try {
+            const response = await fetch(`http://localhost:8081/updateItem/${data.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to delete');
+            }
+
+            console.log('deleted successfully');
+        } 
+        catch (error) {
+            console.error('Error deleting:', error.message);
+        }
+    }};
