adc66d9d3e4fa8b591b9ba892d0328c9b13f1d26
Tate Myers <tatmyers@iastate.edu>
2024-04-27 11:57:46 -0500

    ALL DONE! :D

--- frontend/src/components/ViewProducts.js
+++ frontend/src/components/ViewProducts.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.css";
 
 export function ViewProducts() {
     const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState("");
+    const [productId, setProductId] = useState("");
 
     useEffect(() => {
         fetchProducts();
@@ -15,6 +17,23 @@ export function ViewProducts() {
         // Cleanup function to clear the interval when the component unmounts
         return () => clearInterval(intervalId);
     }, []);
+
+    useEffect(() => {
+        if (productId) { // Only fetch product data if productId is not empty
+            handleDisplayProduct();
+        }
+    }, [productId]);
+
+    const handleDisplayProduct = () => {
+        fetch(`http://localhost:8081/${productId}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(data => setProduct(data))
+        .catch(error => console.error("Error fetching product:", error));
+    };
 
     const fetchProducts = () => {
         fetch("http://localhost:8081/")
@@ -33,6 +52,34 @@ export function ViewProducts() {
 
     return (
         <div className="container">
+            <h2>Search For Product By ID</h2>
+            <div className="container">
+                <div className="mb-3">
+                    <label htmlFor="productId" className="form-label">Product ID:</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="productId"
+                        value={productId}
+                        onChange={(e) => {
+                            setProductId(e.target.value);
+                            handleDisplayProduct(); 
+                        }}
+                    />
+        </div>
+                </div>
+                {product && (
+                    <div>
+                        <div className="col-md-1">
+                            <img src={product.image} className="img-fluid rounded-start" alt={product.title} />
+                        </div>
+                        <h3>Product Details:</h3>
+                        <p>Title: {product.title}</p>
+                        <p>Price: {product.price}</p>
+                        <p>Description: {product.description}</p>
+                        <p>Category: {product.category}</p>
+                    </div>
+                )}
             <h2>All Products</h2>
             {products.map(product => (
                 <div key={product.id} className="card mb-3">
